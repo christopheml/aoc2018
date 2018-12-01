6040064f97e21463a66e219451004c3973eccbd1
Christophe Michel <christopheml@users.noreply.github.com>
2018-12-01 13:49:41 +0100

    [common] Input reader

---
+++ src/main/kotlin/com/github/christopheml/common/PuzzleInput.kt
@@ -0,0 +1,11 @@
+package com.github.christopheml.common
+
+class PuzzleInput(private val filename: String) {
+
+    fun asList(): List<String> {
+        return PuzzleInput::class.java.getResource("/puzzle_inputs/$filename").readText()
+            .lines()
+            .filter { s -> s.isNotBlank() }
+    }
+
+}
