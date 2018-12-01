f127b8cf0899f762ef4a6048bb4a2917adeeac54
Christophe Michel <christopheml@users.noreply.github.com>
2018-12-01 13:49:59 +0100

    [day01] Solution to the first part of the puzzle

--- src/main/kotlin/com/github/christopheml/day01/Calibration.kt
+++ src/main/kotlin/com/github/christopheml/day01/Calibration.kt
@@ -1,5 +1,7 @@
 package com.github.christopheml.day01
 
+import com.github.christopheml.common.PuzzleInput
+
 class Calibration(textFrequencies: List<String>) {
 
     private val frequencies: List<Frequency> = textFrequencies.map { f -> Frequency(f) }
@@ -8,4 +10,10 @@ class Calibration(textFrequencies: List<String>) {
         return frequencies.map { f -> f.value }.sum()
     }
 
+}
+
+fun main(args: Array<String>) {
+    val input = PuzzleInput("day01.txt").asList()
+    val calibration = Calibration(input)
+    println("Solution for first part is " + calibration.frequency())
 }
