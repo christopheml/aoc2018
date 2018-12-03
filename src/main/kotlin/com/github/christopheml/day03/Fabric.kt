ec62194a44fbc0110022240699ce3c4b1b2cfe3a
Christophe Michel <christopheml@users.noreply.github.com>
2018-12-03 08:20:30 +0100

    [day03] Solution to first part

--- src/main/kotlin/com/github/christopheml/day03/Fabric.kt
+++ src/main/kotlin/com/github/christopheml/day03/Fabric.kt
@@ -1,5 +1,7 @@
 package com.github.christopheml.day03
 
+import com.github.christopheml.common.PuzzleInput
+
 internal class Fabric {
 
     private val claimed: MutableMap<Pair<Int, Int>, Int> = HashMap()
@@ -14,4 +16,23 @@ internal class Fabric {
         return claimed.count { entry -> entry.value > 1 }
     }
 
+}
+
+fun main(args: Array<String>) {
+    val splitter = Regex(" @ |: ")
+    val claims = PuzzleInput("day03.txt").asList()
+        .map { s -> splitter.split(s) }
+        .map { s -> toClaim(s[1], s[2]) }
+    val fabric = Fabric()
+    claims.forEach { fabric.claim(it) }
+    println("Solution to first part is " + fabric.claimedTwice())
+}
+
+internal fun toClaim(point: String, size: String): Claim {
+    val coordinates = point.split(",")
+    val dimensions = size.split("x")
+    return Claim(
+        Pair(coordinates[0].toInt(), coordinates[1].toInt()),
+        Pair(dimensions[0].toInt(), dimensions[1].toInt())
+    )
 }
