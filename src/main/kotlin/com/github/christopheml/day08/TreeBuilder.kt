aa2fac36102231f42e75c04bf9394979f307d43c
Christophe Michel <christopheml@users.noreply.github.com>
2018-12-08 11:42:26 +0100

    [day08] Move Node to its own file

--- src/main/kotlin/com/github/christopheml/day08/TreeBuilder.kt
+++ src/main/kotlin/com/github/christopheml/day08/TreeBuilder.kt
@@ -30,30 +30,6 @@ class TreeBuilder(input: Collection<Int>) {
 
 }
 
-class Node(private val children: List<Node>, private val metadata: List<Int>) {
-
-    fun sumOfAllMetadata(): Int {
-        return if (children.isEmpty()) {
-            metadata.sum()
-        } else {
-            metadata.sum() + children.map { it.sumOfAllMetadata() }.sum()
-        }
-    }
-
-    fun valueOf(): Int {
-        return if (children.isEmpty()) {
-            metadata.sum()
-        } else {
-            metadata.mapNotNull { child(it - 1) }.map { it.valueOf() }.sum()
-        }
-    }
-
-    private fun child(index: Int): Node? {
-        return if (index < children.size) children[index] else null
-    }
-
-}
-
 fun main(args: Array<String>) {
     val input = PuzzleInput(8).asString().splitToSequence(" ").map { it.toInt() }.toList()
     val tree = TreeBuilder(input).build()
