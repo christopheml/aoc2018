9ecf151a437a38899b45bc09e5e3af98dbf25f39
Christophe Michel <christopheml@users.noreply.github.com>
2018-12-12 13:54:24 +0100

    [day10] Code cleanup

--- src/main/kotlin/com/github/christopheml/day10/MovingPoint.kt
+++ src/main/kotlin/com/github/christopheml/day10/MovingPoint.kt
@@ -2,7 +2,7 @@ package com.github.christopheml.day10
 
 import com.github.christopheml.common.Point
 
-class MovingPoint(var x: Int, var y: Int, private val vX: Int, private val vY: Int) {
+class MovingPoint(private val x: Int, private val y: Int, private val vX: Int, private val vY: Int) {
 
     fun positionAfter(seconds: Int): Point {
         return Point(x + (seconds * vX), y + (seconds * vY))
