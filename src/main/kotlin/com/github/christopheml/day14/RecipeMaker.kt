920a8ed6497a038f09f1694aa09aca90b2d4e7ef
Christophe Michel <christopheml@users.noreply.github.com>
2018-12-14 13:55:48 +0100

    [day14] Solution to the second part

--- src/main/kotlin/com/github/christopheml/day14/RecipeMaker.kt
+++ src/main/kotlin/com/github/christopheml/day14/RecipeMaker.kt
@@ -1,5 +1,7 @@
 package com.github.christopheml.day14
 
+import com.github.christophem.common.indexOfList
+
 class RecipeMaker(initial: List<Int>) {
 
     private val recipes = initial.toMutableList()
@@ -12,6 +14,18 @@ class RecipeMaker(initial: List<Int>) {
             update()
         }
         return scoreOfLastTen(n)
+    }
+
+    fun runUntilSequence(s: String): Int {
+        val sequence = s.map { it.toString().toInt() }
+        while (true) {
+            update()
+            val snippet = recipes.takeLast(10)
+            val position = snippet.indexOfList(sequence)
+            if (position > -1) {
+                return recipes.size - 10 + position
+            }
+        }
     }
 
     private fun update() {
@@ -43,4 +57,7 @@ class RecipeMaker(initial: List<Int>) {
 fun main(args: Array<String>) {
     var part1 = RecipeMaker(listOf(3, 7)).runFor(919901)
     println("Solution for the first part is $part1")
+
+    var part2 = RecipeMaker(listOf(3, 7)).runUntilSequence("919901")
+    println("Solution for the second part is $part2")
 }
--- src/test/kotlin/com/github/christopheml/day14/RecipeMakerTest.kt
+++ src/test/kotlin/com/github/christopheml/day14/RecipeMakerTest.kt
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.TestFactory
 internal class RecipeMakerTest {
 
     @TestFactory
-    internal fun acceptance_test() = listOf(
+    internal fun part1_acceptance_test() = listOf(
         9 to "5158916779",
         5 to "0124515891",
         18 to "9251071085",
@@ -16,6 +16,20 @@ internal class RecipeMakerTest {
         DynamicTest.dynamicTest("After $input recipes, last ten are $expected") {
             val recipeMaker = RecipeMaker(listOf(3, 7))
             val result = recipeMaker.runFor(input)
+            assertThat(result).isEqualTo(expected)
+        }
+    }
+
+    @TestFactory
+    internal fun part2_acceptance_test() = listOf(
+        "51589" to 9,
+        "01245" to 5,
+        "92510" to 18,
+        "59414" to 2018
+    ).map { (input, expected) ->
+        DynamicTest.dynamicTest("$input first appears after $expected recipes") {
+            val recipeMaker = RecipeMaker(listOf(3, 7))
+            val result = recipeMaker.runUntilSequence(input)
             assertThat(result).isEqualTo(expected)
         }
     }
